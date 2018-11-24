1a4f73c2777d25ab10322c752cb489ecb2ee1538
Pierre Gillon <gillon.pierre69@gmail.com>
2018-11-24 17:16:29 +0900

    Add NotifyIcon package

--- backgroundr.view/backgroundr.view/App.xaml.cs
+++ backgroundr.view/backgroundr.view/App.xaml.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace backgroundr.view
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private TaskbarIcon _notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _notifyIcon.Dispose(); 
+            base.OnExit(e);
+        }
     }
 }
