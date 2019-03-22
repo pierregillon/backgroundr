f4e03c433bdd49e6c75ef8ee3184385715bde55a
Pierre Gillon <gillon.pierre69@gmail.com>
2019-03-22 15:59:32 +0530

    Update tests for more readability

--- backgroundr.tests/TimerFeature.cs
+++ backgroundr.tests/TimerFeature.cs
@@ -15,6 +15,7 @@ namespace backgroundr.tests
         private readonly BackgroundrTimer _timer;
         private readonly IClock _clock;
         private readonly ICommandDispatcher _commandDispatcher;
+        private readonly TimeSpan THREAD_SWITCH_DELAY = TimeSpan.FromMilliseconds(10);
 
         public TimerFeature()
         {
@@ -26,19 +27,21 @@ namespace backgroundr.tests
                 _commandDispatcher
             );
         }
+        ~TimerFeature()
+        {
+            _timer.Stop();
+        }
         
         [Theory]
-        [InlineData("00:00:01")]
-        [InlineData("00:12:13")]
-        [InlineData("12:00:00")]
-        public async Task change_background_instantly_if_refresh_period_expired(string refreshPeriod)
+        [InlineData("22/03/2019", "18/03/2019")]
+        [InlineData("22/03/2019 19:00:00", "22/03/2019 18:00:00")]
+        public async Task ask_new_background_image_instantly_if_time_already_ellapsed(string now, string nextRefreshDate)
         {
             // Arrange
-            var nextRefreshDate = SOME_DATE.Add(TimeSpan.Parse(refreshPeriod));
-            _clock.Now().Returns(nextRefreshDate);
+            _clock.Now().Returns(DateTime.Parse(now));
 
             // Act
-            await _timer.Start(nextRefreshDate);
+            await _timer.Start(DateTime.Parse(nextRefreshDate));
 
             // Assert
             await _commandDispatcher
@@ -47,39 +50,16 @@ namespace backgroundr.tests
         }
 
         [Theory]
-        [InlineData("00:00:05")]
-        [InlineData("00:12:13")]
-        [InlineData("12:00:00")]
-        public async Task do_not_change_background_instantly_if_within_refresh_period(string refreshPeriod)
+        [InlineData("22/03/2019 19:00:00", "22/03/2019 19:00:00.1")]
+        public async Task ask_new_background_image_when_time_ellapsed(string now, string nextRefreshDate)
         {
-            // Data
-            var period = TimeSpan.Parse(refreshPeriod);
-
             // Arrange
-            _clock.Now().Returns(SOME_DATE.Add(period.Divide(2)));
+            _clock.Now().Returns(DateTime.Parse(now));
 
             // Act
-            await _timer.Start(SOME_DATE.Add(period));
-
-            // Assert
-            await _commandDispatcher
-                .Received(0)
-                .Dispatch(Arg.Any<ChangeDesktopBackgroundImageRandomly>());
-        }
-
-        [Fact]
-        public async Task change_background_when_refresh_period_ellapsed()
-        {
-            // Data
-            const int timeBeforeChange = 500;
-            var nextRefreshDate = SOME_DATE.Add(TimeSpan.FromDays(1));
-
-            // Arrange
-            _clock.Now().Returns(nextRefreshDate.Subtract(TimeSpan.FromMilliseconds(timeBeforeChange)));
-
-            // Act
-            await _timer.Start(nextRefreshDate);
-            await Task.Delay(timeBeforeChange);
+            await _timer.Start(DateTime.Parse(nextRefreshDate));
+            var timeDifference = TimeDifference(now, nextRefreshDate);
+            await Task.Delay(timeDifference);
 
             // Assert
             await _commandDispatcher
@@ -87,24 +67,46 @@ namespace backgroundr.tests
                 .Dispatch(Arg.Any<ChangeDesktopBackgroundImageRandomly>());
         }
 
-        [Fact]
-        public async Task stopping_timer_do_not_change_background()
+        [Theory]
+        [InlineData("22/03/2019", "23/03/2019")]
+        [InlineData("22/03/2019 19:00:00", "22/03/2019 20:00:00")]
+        public async Task do_not_ask_for_new_background_image_instantly_if_time_not_ellapsed(string now, string nextRefreshDate)
         {
-            // Data
-            const int timeMs = 50;
-
             // Arrange
-            _clock.Now().Returns(SOME_DATE.Subtract(TimeSpan.FromMilliseconds(timeMs)));
+            _clock.Now().Returns(DateTime.Parse(now));
 
             // Act
-            await _timer.Start(SOME_DATE);
-            await _timer.Stop();
-            await Task.Delay(TimeSpan.FromMilliseconds(timeMs + 10));
+            await _timer.Start(DateTime.Parse(nextRefreshDate));
 
             // Assert
             await _commandDispatcher
                 .Received(0)
                 .Dispatch(Arg.Any<ChangeDesktopBackgroundImageRandomly>());
+        }
+
+        [Theory]
+        [InlineData("22/03/2019 19:00:00", "22/03/2019 19:00:00.1")]
+        public async Task stopping_timer_do_not_ask_new_background_image(string now, string nextRefreshDate)
+        {
+            // Arrange
+            _clock.Now().Returns(DateTime.Parse(now));
+
+            // Act
+            await _timer.Start(DateTime.Parse(nextRefreshDate));
+            await _timer.Stop();
+            await Task.Delay(TimeDifference(now, nextRefreshDate));
+
+            // Assert
+            await _commandDispatcher
+                .Received(0)
+                .Dispatch(Arg.Any<ChangeDesktopBackgroundImageRandomly>());
+        }
+
+        // ----- Utils
+
+        private TimeSpan TimeDifference(string date1, string date2)
+        {
+            return DateTime.Parse(date2).Subtract(DateTime.Parse(date1)).Add(THREAD_SWITCH_DELAY);
         }
     }
 }
