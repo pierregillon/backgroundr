c28e78871a5b7cdff01ad2775619d5d9307efbd7
Pierre Gillon <gillon.pierre69@gmail.com>
2019-03-24 19:50:11 +0400

    Add design time ViewModel

---
+++ backgroundr.view/design/ParametersDesignViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace backgroundr.view.design
+{
+    public class ParametersDesignViewModel
+    {
+        public string UserId { get; set; } = "148722902@N09";
+        public string Tags { get; set; } = "best top";
+        public string Token { get; set; } = "my-token-abcdef1234";
+        public string TokenSecret { get; set; } = "xxxxxxxxxxqfqsdf";
+        public string OAuthAccessToken { get; set; } = "my-oauth-access-token";
+        public string OAuthAccessTokenSecret { get; set; } = "xxxxxxxxaaaaaa";
+        public bool AutomaticallyStart { get; set; } = true;
+    }
+}
