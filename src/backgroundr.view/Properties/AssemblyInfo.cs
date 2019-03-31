8e6453b3b997b42b4113bee66f98fa2e3da1227a
Pierre Gillon <gillon.pierre69@gmail.com>
2019-03-31 13:36:04 +0200

    Rename application properties

--- src/backgroundr.view/Properties/AssemblyInfo.cs
+++ src/backgroundr.view/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("backgroundr.view")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Backgroundr")]
+[assembly: AssemblyDescription("Periodically download a random Flickr photo and set the windows desktop background image.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("backgroundr.view")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("backgroundr-app")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
