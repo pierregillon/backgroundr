ed23f3504e330e0f722e6b3b6b9b0aadd93cc444
Pierre GILLON <gillon.pierre69@gmail.com>
2019-04-06 23:07:44 +0200

    Change background when first time.

---
+++ src/backgroundr.tests/ScheduleNextDesktopBackgroundImageChange.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using backgroundr.application;
+using backgroundr.cqrs;
+using backgroundr.domain;
+using NSubstitute;
+using Xunit;
+
+namespace backgroundr.tests
+{
+    public class ScheduleNextDesktopBackgroundImageChangeTests
+    {
+        private readonly ScheduleNextDesktopBackgroundImageChangeHandler _handler;
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly FlickrParameters _parameters;
+        private IClock _clock;
+
+        public ScheduleNextDesktopBackgroundImageChangeTests()
+        {
+            _parameters = new FlickrParameters();
+            _clock = Substitute.For<IClock>();
+            _commandDispatcher = Substitute.For<ICommandDispatcher>();
+            _handler = new ScheduleNextDesktopBackgroundImageChangeHandler(
+                new CommandDispatchScheduler(_clock, _commandDispatcher, Substitute.For<IFileService>()),
+                _commandDispatcher,
+                _parameters
+            );
+        }
+
+        [Fact]
+        public async Task change_desktop_background_image_instantly_when_first_time()
+        {
+            // Arrange
+            _parameters.BackgroundImageLastRefreshDate = null;
+
+            // Act
+            await _handler.Handle(new ScheduleNextDesktopBackgroundImageChange());
+
+            // Arrange
+            await _commandDispatcher.Received(1).Dispatch(Arg.Any<ChangeDesktopBackgroundImageRandomly>());
+        }
+    }
+}
--- src/backgroundr/application/ScheduleNextDesktopBackgroundImageChangeHandler.cs
+++ src/backgroundr/application/ScheduleNextDesktopBackgroundImageChangeHandler.cs
@@ -6,13 +6,16 @@ namespace backgroundr.application
     public class ScheduleNextDesktopBackgroundImageChangeHandler : ICommandHandler<ScheduleNextDesktopBackgroundImageChange>
     {
         private readonly CommandDispatchScheduler _commandDispatcherScheduler;
+        private readonly ICommandDispatcher _commandDispatcher;
         private readonly FlickrParameters _flickrParameters;
 
         public ScheduleNextDesktopBackgroundImageChangeHandler(
             CommandDispatchScheduler commandDispatcherScheduler,
+            ICommandDispatcher commandDispatcher,
             FlickrParameters flickrParameters)
         {
             _commandDispatcherScheduler = commandDispatcherScheduler;
+            _commandDispatcher = commandDispatcher;
             _flickrParameters = flickrParameters;
         }
 
@@ -22,6 +25,9 @@ namespace backgroundr.application
             if (_flickrParameters.BackgroundImageLastRefreshDate.HasValue) {
                 var nextRefreshDate = _flickrParameters.BackgroundImageLastRefreshDate.Value.Add(_flickrParameters.RefreshPeriod);
                 await _commandDispatcherScheduler.Schedule(new ChangeDesktopBackgroundImageRandomly(), nextRefreshDate);
+            }
+            else {
+                await _commandDispatcher.Dispatch(new ChangeDesktopBackgroundImageRandomly());
             }
         }
     }
